c7497688fe64f538cb39ac52559845477f2d26e2
Sagnik Pratihar <137365977+SagnikPratihar@users.noreply.github.com>
2025-08-25 09:50:29 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = process.env.PORT || 3000;;
+const port = 3000;
 const http = require("http");
 const path = require("path");
 const socketio = require("socket.io");
@@ -26,6 +26,7 @@ app.get("/", (req, res) => {
 });
 
 server.listen(port);
+
 
 
 
